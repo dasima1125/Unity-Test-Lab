cd9d61d77de3a1b965c1fdbb4038e6ab7e353735
dasima1125 <118097194+dasima1125@users.noreply.github.com>
2024-11-08 23:57:28 +0900

    깃 커밋 테스트

---
+++ Assets/scripts/scGitTest.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scGitTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("깃 테스트용");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
