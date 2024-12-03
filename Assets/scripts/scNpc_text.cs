f9169d680c62fb6bcfd3ceff480288e46ffb216c
dasima1125 <118097194+dasima1125@users.noreply.github.com>
2024-12-03 18:29:46 +0900

    json 구조변경
    타입아이디별 접근 시도중

--- Assets/scripts/scNpc_text.cs
+++ Assets/scripts/scNpc_text.cs
@@ -30,7 +30,7 @@ public class scNpc_text : MonoBehaviour
     {
         life    = true;
         talking = false;
-        //jsonReader();
+        jsonReadertest();
         
         
     }
@@ -124,7 +124,8 @@ public class scNpc_text : MonoBehaviour
             JObject json = JObject.Parse(jsonData.text); 
             var insertText = json["npcs"]?.FirstOrDefault(b => b["id"]?.ToString() == id)?["dialogs"]?[dialogs]?.ToObject<string[]>();
             // 출력
-            
+            Debug.Log(string.Join("\n", insertText));
+
             return insertText;
         }
         return null;
@@ -231,5 +232,52 @@ public class scNpc_text : MonoBehaviour
             .transform.Find("ChatBox")
             .gameObject.SetActive(false);
     }
+    IEnumerator talkertest3(TextMeshProUGUI insert)
+    {
+        Debug.Log("테스트 시작");
+        string[] sentences = jsonReadertest();
+        int currentSentenceIndex = 0;
+        
+        
+
+        while (currentSentenceIndex < sentences.Length)
+        {
+            insert.text = "";
+            skip   = false;
+            Debug.Log(currentSentenceIndex + " : 번째");
+            
+            
+            foreach (char letter in sentences[currentSentenceIndex])
+            {   
+                if(skip)
+                {
+                    skip   = false;
+                    insert.text = sentences[currentSentenceIndex];
+                    Debug.Log("skiped");
+                    break;
+                }  
+                
+                insert.text += letter; 
+                yield return new WaitForSeconds(0.05f); 
+
+            }
+            yield return new WaitForSeconds(0.2f);//안정화용 딜레이 
+
+            while (!Input.GetKeyDown(KeyCode.E))// 다음 문장 출력 대기
+            {
+                yield return null; 
+            }
+            Debug.Log("next");
+            currentSentenceIndex++; 
+        }
+        // 마무리단계
+   
+        talking = false; 
+        GameObject.Find("UI_veiwer")
+            .transform.Find("ChatBox")
+            .gameObject.SetActive(false);
+    }
     
 }
+
+
