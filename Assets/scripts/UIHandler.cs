e03dd1e02b0c13ca551f7e6807f6125a92384057
dasima1125 <118097194+dasima1125@users.noreply.github.com>
2025-02-16 21:58:36 +0900

    2025-02-16
    ui 매니저 - 핸들러 - 컨트롤러 구조 분할중

---
+++ Assets/scripts/UIHandler.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] string call_popupID; 
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void OnShowButtonClicked()
+    {
+        Debug.Log("팝업생성 명령");
+        UImanager.UI_Instance.ShowPanel_popup_info(call_popupID);
+    }
+    public void OnHideButtonClicked()
+    {
+        Debug.Log("뒤로가기 명령");
+        UImanager.UI_Instance.HidePanel_popup_info();
+    }
+}
--- Assets/scripts/UImanager.cs
+++ Assets/scripts/UImanager.cs
@@ -23,25 +23,32 @@ public class UImanager : MonoBehaviour
             return instance;  
         }
     }
+    private Dictionary<string, Object> popupUIDictionary = new();//게임 시작시 딱한번 로드하고 저장 팝업ui 오브젝트 저장용
+    private Stack<Object> currentPopupUI = new Stack<Object>(); //씬이 바뀔때는 ui스택을 전부 날려야하기때문에  정적선언 X
     void Awake()
     {
-     
+        
         if (instance != null && instance != this)
         {
             Destroy(gameObject);
-           
         }
         else
         {
             instance = this;
             DontDestroyOnLoad(gameObject);
-     
+            
+            Object[] popupUIs = Resources.LoadAll<Object>("PopupUIs");
+            foreach (Object popupUI in popupUIs)
+            {
+                if (!popupUIDictionary.ContainsKey(popupUI.name)) popupUIDictionary.Add(popupUI.name, popupUI);
+            }
         }
     }
     public GameObject testPanel;
     private GameObject testPanelInstance;
     void Start()
     {
+        Debug.Log("hello world");
         
     }
 
@@ -73,7 +80,56 @@ public class UImanager : MonoBehaviour
 
     public void HidePanel()
     {
+        Debug.Log("왜 작동되는거?");
         testPanelInstance.SetActive(false); //널체크를 딱히 할필요가 없긴해..
+    }
+    public void ShowPanel_popup_info(string uiName)
+    {
+        if (!popupUIDictionary.ContainsKey(uiName)) 
+        {
+            Debug.LogError("존재하지 않는 팝업UI");
+            return;
+        }
+        
+
+        GameObject panelInstance = Instantiate(popupUIDictionary[uiName] as GameObject);
+        panelInstance.transform.SetParent(FindObjectOfType<Canvas>().transform, false);
+        
+        if(currentPopupUI.Count > 0)
+        {
+            Debug.Log("덮어쓰기");
+            GameObject newPosObject = currentPopupUI.Peek() as GameObject;
+            Vector2 newPos = newPosObject.transform.position;
+            panelInstance.transform.position = new Vector2(newPos.x + 20, newPos.y - 20);
+
+        }
+
+        currentPopupUI.Push(panelInstance);
+
+        
+
+        //Vector3 newPosition = panelInstance.transform.position;
+        //Debug.Log(newPosition);
+        //newPosition.x += 10; // 오른쪽으로 10
+        //newPosition.y += 10; // 위로 10
+        //panelInstance.transform.position = newPosition;
+        
+        
+
+        Debug.Log("저장된 작업 스택: " + currentPopupUI.Count);
+
+    }
+    public void HidePanel_popup_info()
+    {
+        if (currentPopupUI.Count < 1) 
+        {
+            Debug.LogError("팝업UI가 씬에 존재하지 않습니다.");
+            return;
+        }
+        GameObject panelInstance = currentPopupUI.Pop() as GameObject;
+       
+        Destroy(panelInstance);
+        Debug.Log("저장된 작업 스택: " + currentPopupUI.Count);
         
     }
 }
